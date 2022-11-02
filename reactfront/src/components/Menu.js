5114dd90b5f7c698abf12a7d61a471a399f7d4f3
Marco <mmardones2021@alu.uct.cl>
2022-11-02 16:43:20 -0300

    AvanceComponentes

--- reactfront/src/components/Menu.js
+++ reactfront/src/components/Menu.js
@@ -14,6 +14,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import SplitButton from 'react-bootstrap/SplitButton';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Accordion from 'react-bootstrap/Accordion';
 
 
 const endpoint ='http://127.0.0.1:8000/api';
@@ -98,85 +99,302 @@ function Menu() {
             </Nav.Item>
           </Nav>
         </div>
-        <div className='espacio'></div>
-        <div className='Info-container' id='divshare'>
-          <p>{myDiv}</p>
-          <p>
-            {myDiv === "Préstamos Biblioteca" && 
-            <table className='componente_A'>
-              <thead>
-                <tr>
-                  <th>Material</th>
-                  <th>Título</th>
-                  <th>Autor</th>
-                  <th>Fecha Préstamo</th>
-                  <th>Fecha Devolución</th>
-                  <th>Biblioteca</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Primer dato</td>
-                  <td>Segundo dato</td>
-                  <td>Tercer dato</td>
-                  <td>Cuarto dato</td>
-                  <td>Quinto dato</td>
-                  <td>Sexto dato</td>
-                </tr>
-              </tbody>
-            </table>}
+<div className='espacio'></div>
+
+<div className='Info-container' id='divshare'>
+  <p>{myDiv}</p>
+
+<p>
+
+{
+//PRÉSTAMOS BIBLIOTECA//
+myDiv === "Préstamos Biblioteca" && 
+  <table className='componente_A'>
+    <thead>
+      <tr>
+        <td>Material</td>
+        <td>Título</td>
+        <td>Autor</td>
+        <td>Fecha Préstamo</td>
+        <td>Fecha Devolución</td>
+        <td>Biblioteca</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td colspan="6">No tiene préstamos vigentes</td>
+      </tr>
+    </tbody>
+    <br></br>
+  </table>}
            
-            {myDiv === "Información Académica" && 
-            <div className='componente_B'>
-              <div className='acordeon'>
-                {data.map((item, i) => (
-                  <div className='item'>
-                    <p>En esta sección encontrarás información académica: cursos inscritos, cursos aprobados, cursos reprobados, competencias genéricas y créditos de libre disposición.</p>
-                    <div className='title' onClick={() =>  toggle(i)}>       
-                      <h4>{item.Historial}</h4>
-                      <span>{selected === i ? '-' : '+'}</span>
-                    </div>
-                    <div className={selected === i ? 'contenido show' : 'contenido'}>{item.Info}
-                    <div>  
-                    </div>
-                    </div>  
-                  </div>
-                ))}
-              </div>
-            </div>}
-          
-          
-          
-            {myDiv === "Notas Parciales" && 
-            <div className='componente_C'>
-            <DropdownButton
-              as={ButtonGroup}           
-              align={{lg: 'end' }}
-              title="Notas Parciales"
-              id="dropdow-menu-align-responsive"
-              >
-            <div>
-              <h1>Información del Estudiante</h1>
-            <tr>
-              <th>Rut: $rut</th>
-              <th>Nombre: $nombre</th>
-            </tr>
-            </div>
-            </DropdownButton> 
-              
-            
-            </div>} 
-          </p>
+{
+// INFORMACIÓN ACADÉMICA // 
+myDiv === "Información Académica" && 
+  <div className='accordion'>
+    <h5>En esta sección encontrarás información académica: cursos inscritos, cursos aprobados, cursos reprobados, competencias genéricas y créditos de libre disposición.
+</h5>
+    <Accordion>
+      <Accordion.Item eventKey="0">
+      <Accordion.Header>Historia Académica</Accordion.Header>
+        <Accordion.Body>
+          <h6>Elige una carrera para consultar las notas de los cursos que has realizado en la Universidad Católica de Temuco.</h6>
+        <div>
+          Información del Estudiante
         </div>
+        <div>
+          <table className='componente_B'>
+            <thead>
+              <tr>
+                <td>Rut</td>
+                <td>Nombre</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Dato1</td>
+                <td>Dato2</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <h5>Seleccione su carrera:</h5>
+          <DropdownButton
+            as={ButtonGroup}           
+            align={{lg: 'up' }}
+            title="Lista de carreras..."
+            id="dropdown-item-button"
+          >
+            <Dropdown.Item>
+              Aquí van las carreras
+            </Dropdown.Item>
+          </DropdownButton>
+        </div>
+        </Accordion.Body>
+      </Accordion.Item>
+        <Accordion.Item eventKey="1">
+        <Accordion.Header>Cursos Inscritos</Accordion.Header>
+        <Accordion.Body>
+          <h6>Al seleccionar una carrera se cargará el año y el semestre actual que estas cursando, sin embargo, puedes modificar el año y el semestre para consultar otras cargas académicas. Al hacer click en el boton 'Generar Certificado', se genererá un archivo con extension .PDF que posee toda la información de tus cursos inscritos.</h6>
+          <br></br>
+        <div>
+          <h5>Seleccione su carrera:</h5>
+          <DropdownButton
+            as={ButtonGroup}           
+            align={{lg: 'up' }}
+            title="Lista de carreras..."
+            id="dropdown-item-button"
+          >
+            <Dropdown.Item>
+              Aquí van las carreras
+            </Dropdown.Item>
+          </DropdownButton>
+        </div>
+        </Accordion.Body>
+        </Accordion.Item>
+    </Accordion>   
+  </div>}
+          
+               
+{
+//NOTAS PARCIALES------------------------------------------------
+
+myDiv === "Notas Parciales" && 
+  <div className='accordion'>
+    <Accordion>
+      <Accordion.Item eventKey="0">
+      <Accordion.Header>Notas Parciales</Accordion.Header>
+        <Accordion.Body>
+          <h6>En esta sección encotrarás las notas parciales y los procentajes de asistencia de las carreras.</h6>
+        <div>
+          Información del Estudiante
+        </div>
+        <table className='componente_C'>
+          <thead>
+            <tr>
+              <td>Rut</td>
+              <td>Nombre</td>
+            </tr>
+          </thead>
+          <tbody id='datos'>
+            <tr>
+              <td>Dato1</td>
+              <td>Dato2</td>
+            </tr>
+          </tbody>
+        </table>
+        <h5>Seleccione una Carrera:</h5>
+            
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
+      <h5>Seleccione Semestre:</h5>
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Seleccione Semestre
+        </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-2">Primer Semestre</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Segundo Semestre</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+      <Accordion.Header>Asistencia</Accordion.Header>
+        <Accordion.Body>
+        <div>
+          Información del Estudiante
+        </div>
+        <table className='componente_C'>
+          <thead>
+            <tr>
+              <td>Rut</td>
+              <td>Nombre</td>
+            </tr>
+          </thead>
+          <tbody id='datos'>
+            <tr>
+              <td>Dato1</td>
+              <td>Dato2</td>
+            </tr>
+          </tbody>
+        </table>
+        <h5>Seleccione una Carrera:</h5>
+            
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
+      <h5>Seleccione Semestre:</h5>
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Seleccione Semestre
+        </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-2">Primer Semestre</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Segundo Semestre</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+      
+  </div>}
+
+{
+//CUENTA CORRIENTE//
+myDiv === "Cuenta Corriente" && 
+  <div className='accordion'>
+    <h6>En esta sección encontrarás información de tus cuentas (Deudas, Pagarés, cuotas, etc) y beneficios (Becas y/o Créditos) que posees en la Universidad Católica de Temuco.</h6>
+      <Accordion>
+      <Accordion.Item eventKey="0">
+      <Accordion.Header>Cuenta Corriente</Accordion.Header>
+        <Accordion.Body>
+        <h4>Seleccione una Carrera:</h4>      
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+      
+      <Accordion.Item eventKey="1">
+      <Accordion.Header>Pago con WebPay Plus</Accordion.Header>
+        <Accordion.Body>
+        <h4>Seleccione una Carrera:</h4>      
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+      
+      <Accordion.Item eventKey="2">
+      <Accordion.Header>Beneficios Estudiantiles</Accordion.Header>
+        <Accordion.Body>
+        <h4>Seleccione una Carrera:</h4>      
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+      
+      <Accordion.Item eventKey="3">
+      <Accordion.Header>Crédito Complementario</Accordion.Header>
+        <Accordion.Body>
+        <h4>Seleccione una Carrera:</h4>      
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+      
+      <Accordion.Item eventKey="4">
+      <Accordion.Header>Emisión de Documentos</Accordion.Header>
+        <Accordion.Body>
+        <h4>Seleccione una Carrera:</h4>      
+      <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>           
+  </div>}
+
+  {
+//HORAS ASISTENTE SOCIAL//
+myDiv === "Horas Asistente Social" && 
+  <div>
+  <h6>En esta sección podrás realizar una reserva de hora con un/a asistente social donde se realizan las clases de tu carrera. Debes considerar que una reserva debe hacerse con 24 horas de antelación (de Lunes a Viernes) y sólo se podrá realizar una reserva cada 15 días a través de este medio. La atención será a través de videollamada, ya que se encuentran suspendidas las atenciones presenciales de forma indefinida</h6>
+  <Dropdown>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          Lista de carreras...
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-2">carrera 1</Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
+  </div>}
+
+</p>
+</div>
         </>
       );
 }
-
-const data = [
-  {
-    Historial: 'Historia Académica',
-    Info: 'Información del Estudiante'
-  }
-]
 
 export default Menu;
